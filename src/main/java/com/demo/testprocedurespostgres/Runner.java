582b2935c4b9a08443b44763db028f713b2fb35f
AndrejKlim <tydysch@mail.ru>
2020-05-28 10:22:24 +0300

    Added email sending

--- src/main/java/com/demo/testprocedurespostgres/Runner.java
+++ src/main/java/com/demo/testprocedurespostgres/Runner.java
@@ -1,23 +1,16 @@
 package com.demo.testprocedurespostgres;
 
-import com.demo.testprocedurespostgres.entity.CalcResult;
 import com.demo.testprocedurespostgres.entity.SalePoint;
-import com.demo.testprocedurespostgres.repo.AsyncRunner;
-import com.demo.testprocedurespostgres.repo.CalcResultRepo;
-import com.demo.testprocedurespostgres.repo.EmpRepo;
 import com.demo.testprocedurespostgres.repo.SalePointRepo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityManager;
-import java.util.ArrayList;
 import java.util.Random;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -27,31 +20,44 @@ public class Runner implements CommandLineRunner {
 	@Autowired
 	SalePointRepo salePointRepo;
 	@Autowired
-	CalcResultRepo calcResultRepo;
+	JavaMailSender javaMailSender;
 	@Autowired
 	JdbcTemplate jdbcTemplate;
-	@Autowired
-	AsyncRunner asyncRunner;
+
 
 	@Override
 	public void run(String... args) throws Exception {
 		final Random random = new Random();
-//		int [] dataRecords = {0, 10000, 50000, 100000, 500000, 1000000, 2000000, 3000000,
-//								5000000, 8000000, 10000000, 12000000, 15000000, 17000000, 20000000};
+		sendEmailMessage("Start", "Procedure start processing db");
 		log.info("Truncating prize db");
 		jdbcTemplate.execute("truncate \"testTaskDB\".public.sale_point_prize;");
-		log.info("Start Processing 4 procedures");
-		CompletableFuture<Long> time1 = asyncRunner.runDBProcedure(1);
-		CompletableFuture<Long> time2 = asyncRunner.runDBProcedure(2);
+		log.info("Starting processing procedure");
+		long start = System.currentTimeMillis();
+		salePointRepo.proxyAlterCalcPrize();
+		long fin = System.currentTimeMillis();
+		long operationTime = fin-start;
+		log.info("Time to process procedure - {} ms, {} s, {} min in thread {}",
+				operationTime,
+				TimeUnit.MILLISECONDS.toSeconds(operationTime),
+				TimeUnit.MILLISECONDS.toMinutes(operationTime),
+				Thread.currentThread().getName());
+		sendEmailMessage("End", "Procedure finished processing db");
+//		int [] dataRecords = {0, 10000, 50000, 100000, 500000, 1000000, 2000000, 3000000,
+//								5000000, 8000000, 10000000, 12000000, 15000000, 17000000, 20000000};
+//		log.info("Truncating prize db");
+//		jdbcTemplate.execute("truncate \"testTaskDB\".public.sale_point_prize;");
+//		log.info("Start Processing 4 procedures");
+//		CompletableFuture<Long> time1 = asyncRunner.runDBProcedure(1);
+//		CompletableFuture<Long> time2 = asyncRunner.runDBProcedure(2);
 //		CompletableFuture<Long> time3 = asyncRunner.runDBProcedure(3);
 //		CompletableFuture<Long> time4 = asyncRunner.runDBProcedure(4);
 //
-		CompletableFuture.allOf(time1, time2).join();
-
-		CompletableFuture<Long> time3 = asyncRunner.runDBProcedure(3);
-		CompletableFuture<Long> time4 = asyncRunner.runDBProcedure(4);
-
-		log.info("time 1 = {}",time1.get());
+//		CompletableFuture.allOf(time1, time2).join();
+//
+//		CompletableFuture<Long> time3 = asyncRunner.runDBProcedure(3);
+//		CompletableFuture<Long> time4 = asyncRunner.runDBProcedure(4);
+//
+//		log.info("time 1 = {}",time1.get());
 
 		//log.info("Save result to DB, records = {}, time = {} ms", 10000000, operationTime);
 		//calcResultRepo.save(new CalcResult(10000000, operationTime));
@@ -103,5 +109,13 @@ public class Runner implements CommandLineRunner {
 		soldModem = random.nextInt(2000);
 		soldTvAdapters = random.nextInt(1000);
 		return new SalePoint(pointName, place, soldSim, soldModem, soldTvAdapters);
+	}
+
+	private void sendEmailMessage(String subject,String text){
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo("tydysch@mail.ru");
+		message.setSubject(subject);
+		message.setText(text);
+		javaMailSender.send(message);
 	}
 }
--- src/main/java/com/demo/testprocedurespostgres/TestprocedurespostgresApplication.java
+++ src/main/java/com/demo/testprocedurespostgres/TestprocedurespostgresApplication.java
@@ -1,14 +1,13 @@
 package com.demo.testprocedurespostgres;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class TestprocedurespostgresApplication {
 
 	public static void main(String[] args) {
